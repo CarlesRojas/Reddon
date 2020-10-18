cf030816910fefc0cf8bf75d2360c81ceac66490
Carles <carles.gstocks@gmail.com>
2020-10-18 12:28:03 +0200

    Text subreddits

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,4 +1,5 @@
 import React, { useContext } from "react";
+import ReactHtmlParser from "react-html-parser";
 
 // Contexts
 import { Utils } from "contexts/Utils";
@@ -15,8 +16,9 @@ export default function Post(props) {
     const { unixTimeToDate, timeAgo } = useContext(Utils);
     const { subredditsInfo } = useContext(Reddit);
 
+    console.log(postData);
     // Post data
-    const { subreddit, subreddit_id, author, title, ups, created_utc, preview, media } = postData;
+    const { subreddit, subreddit_id, author, title, selftext_html, score, created_utc, preview, media, over_18, hidden, likes, locked } = postData;
 
     // Subreddit icon
     if (
@@ -27,12 +29,17 @@ export default function Post(props) {
         var subredditIcon = <img className="subredditIcon" src={subredditsInfo.current[subreddit_id].icon_img} alt=""></img>;
     } else subredditIcon = <img className="subredditIcon" src={ReddonLogo} alt=""></img>;
 
+    // Text for the post
+    var selfText = selftext_html ? <div className="selfText">{ReactHtmlParser(selftext_html)}</div> : null;
+
     return (
         <div className="post">
             {subredditIcon}
             <p className="subreddit">{subreddit}</p>
             <p className="author">{author + " · " + timeAgo(unixTimeToDate(created_utc), false)}</p>
             <p className="title">{title}</p>
+
+            {selfText}
         </div>
     );
 }
